2c59b0af042cce547fd2891cfa7e113e7c2c42be
xmdhs <xmdhss@gmail.com>
2024-01-16 18:03:07 +0800

    增加日志

--- main.go
+++ main.go
@@ -71,6 +71,7 @@ func dosome(ctx context.Context, q *qbittorrent.Qbit, banPeerIdReg *regexp.Regex
 					needBanMapL.Lock()
 					needBanMap[v.IP] = expiredTime
 					needBanMapL.Unlock()
+					log.Println(v.IP, v.PeerIdClient)
 				}
 			}
 			return nil
