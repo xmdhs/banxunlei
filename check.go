3a2b0d278d99b4afde27abba283e0528b0b75314
xmdhs <xmdhss@gmail.com>
2024-04-04 23:26:05 +0800

    更改进度判断参数

--- check.go
+++ check.go
@@ -16,13 +16,13 @@ func peerIdCheck(banPeerIdReg, banClientReg *regexp.Regexp) check {
 
 func progressCheck(totalSize int) check {
 	return func(p qbittorrent.Peer) bool {
-		if p.Uploaded < 30*1000*1000 {
+		if p.Uploaded < min(100*1000*1000, int(float64(totalSize)*0.1)) {
 			return false
 		}
 		if p.Uploaded+50*1000*1000 > totalSize {
 			return true
 		}
-		if float64(p.Uploaded)+50*1000*1000 > p.Progress*float64(totalSize) {
+		if float64(p.Uploaded) > p.Progress*float64(totalSize)+min(300*1000*1000, float64(totalSize)*0.1) {
 			return true
 		}
 		return false
